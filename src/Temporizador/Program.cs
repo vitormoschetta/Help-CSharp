c2bca9187f39827acb1db11e3f782acf07fc0537
vitormoschetta <vitormoschetta@gmail.com>
2020-09-17 00:34:30 -0400

    add temporizador

---
+++ src/Temporizador/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Temporizador
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Clique para Iniciar");
+			var a = Console.ReadLine();
+			Processador.processar();
+        }
+    }
+}
