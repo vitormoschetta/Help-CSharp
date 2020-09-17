3a06d232ba1255eecba091772bee68981834b9f1
vitormoschetta <vitormoschetta@gmail.com>
2020-09-17 00:57:04 -0400

    add Datas

---
+++ src/Datas/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Datas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = new DateTime(2020, 9, 16);
+
+
+            // Data e hora
+            Console.WriteLine(data);
+
+            // Data
+            Console.WriteLine(data.ToString("d"));
+
+            // Data por extenso
+            Console.WriteLine(data.ToLongDateString());
+
+            // Data atual
+            Console.WriteLine(DateTime.Today);
+
+            // Data atual - 2 dias
+            Console.WriteLine(DateTime.Today.AddDays(-2));
+
+            // quantos dias o ano teve até agora:
+            var dataAtual = DateTime.Today;
+            var inicio = new DateTime(2020, 1, 1);
+            var dias = (dataAtual - inicio).Days;
+            Console.WriteLine(dias);
+
+            // Customizados
+            Console.WriteLine(data.ToString("dd . MM . yyyy"));
+            Console.WriteLine(data.ToString("dd/MM/yyyy  hh:mm:ss"));
+            Console.WriteLine(data.ToString("MM/yyyy"));
+
+        }
+    }
+}
