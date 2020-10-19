386eb6f09c52ca7254b50ad43d299622789e25b8
vitormoschetta <vitormoschetta@gmail.com>
2020-10-19 17:44:10 -0400

    add Math

---
+++ src/Math/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _Math
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Diferença de dois inteiros sempre em um inteiro POSITIVO:
+            int x = 15;
+            int y = 5;
+
+            var result = Math.Abs(y - x);
+
+            Console.WriteLine(result);
+        }
+    }
+}
