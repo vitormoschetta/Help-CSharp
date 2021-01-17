af345cbf06e7fe3e1b5ce7448d6740dfb688810f
vitormoschetta <vitormoschetta@gmail.com>
2021-01-17 14:17:26 -0400

    regex

--- src/RegexUser/Program.cs
+++ src/RegexUser/Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿// 
 using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
@@ -20,14 +20,24 @@ namespace RegexUser
             // Exemplos.ProcurarOcorrenciaDeTextos09();
             // Exemplos.ProcurarOcorrenciaDeTextos10();
 
-            var Exemplos = new Exemplos02();
+            // var Exemplos = new Exemplos02();
             // Exemplos.ProcurarOcorrenciaDeTextos();
             // Exemplos.ProcurarOcorrenciaDeTextos02();
             // Exemplos.ProcurarOcorrenciaDeTextos03();
             // Exemplos.ProcurarOcorrenciaDeTextos04();
             // Exemplos.ProcurarOcorrenciaDeTextos05();
             // Exemplos.ProcurarOcorrenciaDeTextos06();
-            Exemplos.ProcurarOcorrenciaDeTextos07();
+            // Exemplos.ProcurarOcorrenciaDeTextos07();
+
+            // var Exemplos = new Exemplos03();
+            // Exemplos.ExemploA();
+
+            var estudd01 = new Estudo01();
+            // estudd01.Exercicio01();
+            // estudd01.Exercicio02();
+            // estudd01.Exercicio03();
+            estudd01.Exercicio04();
+
         }
     }
 
