67fc39e217235a79b03e393e82cd1ef23dc05432
=vitormoschetta <=vitormoschetta@gmail.com>
2021-02-22 21:30:07 -0400

    update yield result

---
+++ src/Yield/Content.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace Yield
+{
+    public class Content
+    {
+        public List<int> Items { get; set; }
+        public int TotalPages { get; set; }
+        public int CurrentPage { get; set; }
+    }
+}
--- src/Yield/Program.cs
+++ src/Yield/Program.cs
@@ -1,15 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace Yeld
+namespace Yield
 {
     class Program
     {
         static void Main(string[] args)
         {
             int sizePage = 50;
-            var db = new Repository();            
-            int totalItens = db.Count();                  
+            var repository = new Repository();            
+            int totalItens = repository.Count();                  
 
             var content = new Content(); 
             content.Items = new List<int>();      
@@ -17,7 +17,12 @@ namespace Yeld
 
             int currentPage = 1;                         
 
-            foreach (var item in db.Get())
+
+            // Podemos usar tanto o repository.Get01 quanto o Get02.
+            // A diferença é que o Get02 utiliza 'yield return', retornando os dados 
+            // linha a linha a medida que vão ficando disponíveis, ganhando em performance, 
+            // pois não deixa serviços ociosos.            
+            foreach (var item in repository.Get02())
             {                
                 content.Items.Add(item);                
 
@@ -44,48 +49,6 @@ namespace Yeld
             Console.WriteLine("\n");
         }
     
-    }
-
-
-    public class Repository
-    {
-        private int[] list = new int[200];
-        public Repository()
-        {
-            for (int i = 0; i < 200; i++)
-            {
-                list[i] = i;
-            }
-        }
-
-
-        public int Count()
-        {
-            return list.Length;
-        }
-
-
-        public IEnumerable<int> Get()
-        {
-            //int[] list = new int[200];
-
-            for (int i = 0; i < list.Length; i++)
-            {
-                //list[i] = i;
-                yield return list[i];
-            }
-
-            // return list;
-            
-        }
-        
-    }
-
-
-    public class Content
-    {
-        public List<int> Items { get; set; }
-        public int TotalPages { get; set; }
-        public int CurrentPage { get; set; }
-    }
+    }    
+    
 }
