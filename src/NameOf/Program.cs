421f9de652a79f94493f874547e41c1e7f09e52d
=vitormoschetta <=vitormoschetta@gmail.com>
2021-02-24 16:45:46 -0400

    add nameoff

---
+++ src/NameOf/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NameOf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var product = new Product(null);
+        }
+       
+    }
+    
+}
