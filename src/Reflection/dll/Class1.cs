8ae2f1f97197f51059c6ab2033492d7da4801664
Vitor <vfarias@bionexo.com>
2021-04-05 22:59:36 -0400

    add reflection

---
+++ src/Reflection/dll/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace dll
+{
+    public class Metodos
+    {
+        public static decimal Result { get; set; }
+        public static void Somar(decimal x, decimal y)
+        {
+            Result = x + y;
+            Console.WriteLine(Result);
+        }
+
+        public static void Subtrair(decimal x, decimal y)
+        {
+            Result = x - y;
+            Console.WriteLine(Result);
+        }
+    }
+}
