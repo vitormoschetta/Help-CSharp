81e7b259d57a5510266b45e230db5abd60536260
Vitor <vfarias@bionexo.com>
2021-04-05 22:28:54 -0400

    updating Tasks

--- src/Tasks/Program.cs
+++ src/Tasks/Program.cs
@@ -8,6 +8,12 @@ namespace Tasks
     {
         static void Main(string[] args)
         {
+            ExecuteMetodos();
+            //ExecuteMetodosComRetorno();
+        }
+
+        static void ExecuteMetodos()
+        {
             // Apenas uma Thread executando os métodos de forma síncrona:
             Metodos.MetodoA();
             Metodos.MetodoB();
@@ -15,7 +21,7 @@ namespace Tasks
 
 
             Console.WriteLine("\n");
-                      
+
 
 
             // Executando as Tasks de forma assíncrona:
@@ -33,6 +39,19 @@ namespace Tasks
             Console.WriteLine("\n");
 
 
+            // Outra forma de executar Tasks de forma assíncrona:
+            var tarefas = new Task[3];
+
+            tarefas[0] = Task.Run(() => Metodos.MetodoA());
+            tarefas[1] = Task.Run(() => Metodos.MetodoB());
+            tarefas[2] = Task.Run(() => Metodos.MetodoC());
+
+            Task.WaitAll(tarefas);
+
+
+            Console.WriteLine("\n");
+
+
             // Executando as Tasks de forma síncrona:
             taskA = new Task(Metodos.MetodoA);
             taskB = new Task(Metodos.MetodoB);
@@ -44,8 +63,23 @@ namespace Tasks
             taskB.Start();
             taskB.Wait();
 
-            taskC.Start();                    
+            taskC.Start();
             taskC.Wait();
+            
+        }
+
+
+
+        static void ExecuteMetodosComRetorno()
+        {
+            // Apenas uma Thread executando os métodos de forma síncrona:
+            Console.WriteLine(MetodosComRetorno.MetodoA().Result);
+            Console.WriteLine(MetodosComRetorno.MetodoB().Result);
+            Console.WriteLine(MetodosComRetorno.MetodoC().Result);
+
+
+            Console.WriteLine("\n");
+        
         }
     }
 }
