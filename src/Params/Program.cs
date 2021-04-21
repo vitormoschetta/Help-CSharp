c9d0b982f75cadac1abd74c7293be88d2781be93
Vitor <vfarias@bionexo.com>
2021-04-21 14:53:33 -0400

    add params

---
+++ src/Params/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Params
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new int[5] { 1, 2, 3, 4, 5 };
+
+            UseParams(1, 2, 3, 4, 5, 'a');
+            NotUseParams(array);
+        }
+        public static void NotUseParams(int[] list)
+        {
+            for (int i = 0; i < list.Length; i++)
+            {
+                Console.Write(list[i] + " ");
+            }
+            Console.WriteLine();
+        }
+
+
+        // a palavra reservada 'params' coloca todos os parametros enviados em um array:
+        public static void UseParams(params dynamic[] list)
+        {
+            for (int i = 0; i < list.Length; i++)
+            {
+                Console.Write(list[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
