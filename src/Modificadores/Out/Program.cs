e246fe973ba292a21c2d83cbe77af3b2e520de61
Vitor <vfarias@bionexo.com>
2021-04-21 15:34:35 -0400

    update structure projetc

---
+++ src/Modificadores/Out/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Out
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            RaizQuadrada(out int number);
+            Console.WriteLine(number); // outpub: 1989         
+        }
+
+        
+        // Aqui a variavel 'number' tbm é passada como refência, porém ao utilizar a palavra reservada
+        // 'out', podemos passar a varável sem precisá-la iniciar (atribuir valor).
+        static void RaizQuadrada(out int number)
+        {
+            number = 1989;
+        }
+    }
+}
