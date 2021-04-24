0074e93cd4aa11216b640750e1fd7976ddd7e3da
Vitor <vfarias@bionexo.com>
2021-04-24 19:24:34 -0400

    add lambda expressions

--- src/_Lambda/Program.cs
+++ src/_Lambda/Program.cs
@@ -2,11 +2,47 @@
 
 namespace _Lambda
 {
+
+    // Na classe que se segue, além do método Main, existem outras três funções que 
+    // fazem a mesma coisa..
+
+
     class Program
     {
+        private static int result;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            result = CalcSquareA(5);
+            Console.WriteLine(result);
+
+            result = CalcSquareB(5);
+            Console.WriteLine(result);
+
+            result = CalcSquareC.Invoke(5);
+            Console.WriteLine(result);
         }
+
+
+
+        // Sintaxe mais comum, como a maioria dos programadores estão acostumados:
+        static int CalcSquareA(int x)
+        {
+            return x * x;
+        }
+
+
+
+        // Sintaxe de função utilizando Expressão Lambda:
+        static Func<int, int> CalcSquareB = x => x * x;        
+
+
+
+        // Sintaxe utilizando delegate     
+        static DelCalcSquare CalcSquareC = (x) => { return x * x; };
+
+        // Para o caso acima é preciso declarar um delegate:
+        public delegate int DelCalcSquare(int x);
+
+
     }
 }
