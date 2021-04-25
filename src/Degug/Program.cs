6a7915adf120d11d475b2b6796604ed6dc665c35
Vitor <vfarias@bionexo.com>
2021-04-25 12:45:56 -0400

    update dict lambda use

--- src/Degug/Program.cs
+++ src/Degug/Program.cs
@@ -7,69 +7,15 @@ namespace Degug
 {
     class Program
     {
-        // Problema: imprimir, em uma única linha, cada número da lista de 'numbers' ao quadrado.
-        static int[] numbers = { 2, 3, 4, 5 };
 
         static void Main(string[] args)
         {
-            Solution01();
-            Solution02();
-            Solution03();
-            Solution04();
+            Console.WriteLine(testForEquality(1,1));
         }
 
-
-        // Resolvendo com For
-        static void Solution01()
-        {
-            var numbersSquare = new int[4];
-
-            for (int i = 0; i < numbers.Length; i++)
-            {
-                numbersSquare[i] = numbers[i] * numbers[i];
-            }
-
-            Console.WriteLine(string.Join(" ", numbersSquare));
-        }
-
-
-        // Resolvendo com Foreach
-        static void Solution02()
-        {
-            var numbersSquare = new int[4];
-            int i = 0;
-
-            foreach (var item in numbers)
-            {
-                numbersSquare[i] = item * item;
-                i++;
-            }
-
-            Console.WriteLine("\n"+ string.Join(" ", numbersSquare));
-        }
-
-
-        // Resolvendo com Lambda Expressions
-        static void Solution03()
-        {
-            var numbersSquare = numbers.Select(x => x * x);  
-
-            Console.WriteLine("\n"+ string.Join(" ", numbersSquare));
-        }
-
-
-        // Resolvendo com Linq
-        static void Solution04()
-        {
-            var numbersSquare = from item in numbers 
-                                select item * item;
-
-            Console.WriteLine("\n"+ string.Join(" ", numbersSquare));
-        }
+        static Func<int, int, bool> testForEquality = (x, y) => x == y;
         
 
-
-        //Obs: As soluções Linq e Lambda utilizam um loop por baixo, como se fosse um for ou foreach.
     }
 
 }
--- src/_Collections/_Dictionary/Program.cs
+++ src/_Collections/_Dictionary/Program.cs
@@ -1,52 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace _Dictionary
 {
     class Program
     {
+        static Dictionary<string, string> dict = new Dictionary<string, string>();
         static void Main(string[] args)
         {
-            Dictionary<string, string> dict = new Dictionary<string, string>();                      
-
-            dict.Add("01", "C#");
-            dict.Add("02", "Python");
-            dict.Add("03", "NodeJS");
-
             try
             {
+                SetDict();
+                ExploreDict();
+                ExploreDictLinq();
+
+                // A key 03 já existe no Dicionário, ao tentar adicionar novamente levantamos uma exeção            
                 dict.Add("03", "Ruby");
-                // Ao tentar adicionar novamente a Key '03' vai levantar uma exceção
             }
             catch (Exception ex)
-            {                
-                Console.WriteLine(ex.Message);
-            }
-            finally 
             {
-                Console.WriteLine(string.Join("; ", dict));
-                // Aqui  imprime os valores que foram adicionados com sucesso..
-
-                ExploreDict(dict);
+                Console.WriteLine(ex.Message);
             }
         }
 
-        static void ExploreDict(Dictionary<string, string> dict)
+
+        static void SetDict()
+        {
+            dict.Add("01", "C#");
+            dict.Add("02", "Python");
+            dict.Add("03", "NodeJS");
+        }
+
+        static void ExploreDict()
         {
             Console.WriteLine("\n");
+
             Console.WriteLine(string.Join("; ", dict.Keys));
             Console.WriteLine(string.Join("; ", dict.Values));
 
-            Console.WriteLine(dict.ContainsKey("04"));      
-            Console.WriteLine(dict.ContainsValue("C#"));       
+            Console.WriteLine(dict.ContainsKey("04"));
+            Console.WriteLine(dict.ContainsValue("C#"));
 
-            Console.WriteLine(dict.Count);  
-            
+            Console.WriteLine(dict.Count);
+
             dict.Remove("01");
-            Console.WriteLine(string.Join("; ", dict));  
+            Console.WriteLine(string.Join("; ", dict.Values));
 
             dict.TryGetValue("02", out string valor);
-            Console.WriteLine(valor);                        
+            Console.WriteLine(valor);
+        }
+
+
+        static void ExploreDictLinq()
+        {
+            Console.WriteLine("\n");
+
+            Console.WriteLine(dict.FirstOrDefault(x => x.Key == "02"));
         }
     }
 }
