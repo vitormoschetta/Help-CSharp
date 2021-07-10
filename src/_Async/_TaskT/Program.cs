c189f29d401014302b2bf8d16ec170da5c96612f
Vitor <vfarias@bionexo.com>
2021-07-10 01:30:54 -0400

    add task <t> await

---
+++ src/_Async/_TaskT/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace _TaskT
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var html = await GetHtmlAsync();
+            Console.WriteLine(html);
+
+            ProcessoY();
+            Console.ReadLine();
+        }
+
+        static async Task<string> GetHtmlAsync()
+        {
+            Console.WriteLine("\nIniciando GetHtml");
+
+            string html = await new HttpClient().GetStringAsync("https://vitormoschetta.github.io/");
+            return html;
+        }
+
+        static void ProcessoY()
+        {
+            Console.WriteLine("\nIniciando processo Y...");
+            Console.WriteLine("\nFim do processo Y");
+        }
+    }
+}
