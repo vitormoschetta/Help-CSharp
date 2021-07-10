ba25e5dfb46d921d65178d2a2c7f755b3b2579f3
Vitor <vfarias@bionexo.com>
2021-07-10 01:37:56 -0400

    add await task.run

---
+++ src/_Async/_AsyncTaskRun/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _AsyncTaskRun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // como o processo X é async, a Thread não aguarda a sua conclusão e executa o processo Y 
+            ProcessoX();
+
+            ProcessoY();
+            Console.ReadLine();
+        }
+
+        static async void ProcessoX()
+        {
+            // caso não de para utilizar a palavra podemos usar o await Task.Run em substituição ao await:         
+            await Task.Run(() =>
+            {
+                Console.WriteLine("Iniciando processo X...");
+                Thread.Sleep(5000);
+                Console.WriteLine("Fim do processo X");
+            });
+
+        }
+
+        static void ProcessoY()
+        {
+            Console.WriteLine("Iniciando processo Y...");
+            Console.WriteLine("Fim do processo Y");
+        }
+    }
+}
